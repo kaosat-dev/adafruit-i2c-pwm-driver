31e32281304408de323a3e26226824485c6b1866
Mark Moissette <kaosat.dev@gmail.com>
2016-11-05 10:26:35 +0000

    feat(i2c wrapper): added a promise based wrapper around the i2c library

---
+++ src/i2cWrapper.js
@@ -0,0 +1,45 @@
+const I2C = require('i2c')
+
+function makeI2CWrapper (address, {device, debug}) {
+  const i2c = new I2C(address, {device})
+
+  const readBytes = (cmd, length) => {
+    return new Promise(
+      function (resolve, reject) {
+        i2c.readBytes(cmd, length, function (error, data) {
+          if (error) {
+            return reject(error)
+          }
+          resolve(data)
+        })
+      }
+    )
+  }
+
+  const writeBytes = (cmd, buf) => {
+    if (!(buf instanceof Array)) {
+      buf = [buf]
+    }
+    if(debug){
+      console.log(`cmd ${cmd.toString(16)} values ${buf}`)
+    }
+    return new Promise(
+      function (resolve, reject) {
+        i2c.writeBytes(cmd, buf, function (error, data) {
+          if (error) {
+            return reject(error)
+          }
+
+          resolve(data)
+        })
+      }
+    )
+  }
+
+  return {
+    readBytes,
+    writeBytes
+  }
+}
+
+module.exports = makeI2CWrapper
