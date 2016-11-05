07c2c75f05d446f135012cf299241b3e971d4ad3
Mark Moissette <kaosat.dev@gmail.com>
2016-11-05 10:26:01 +0000

    chore(examples): added basic example (wip)

---
+++ examples/simple.js
@@ -0,0 +1,24 @@
+const makePwmDriver = require('../src/index')
+// const makePwmDriver = require('adafruit-i2c-pwm')
+const sleep = require('sleep').sleep
+
+const pwm = makePwmDriver(0x40, '/dev/i2c-1')
+
+// Configure min and max servo pulse lengths
+const servo_min = 150 // Min pulse length out of 4096
+const servo_max = 600 // Max pulse length out of 4096
+
+pwm.setPWMFreq(50)
+
+setTimeout(function () {
+  console.log('Moving servo on channel 0, press Ctrl-C to quit...')
+  while(true) {
+    // Move servo on channel O between extremes.
+    pwm.setPWM(0, 0, servo_min)
+    //pwm.setPWM(12, 0, servo_min)
+    sleep(1)
+    pwm.setPWM(0, 0, servo_max)
+    //pwm.setPWM(12, 0, servo_max)
+    sleep(1)
+  }
+}, 5000)
